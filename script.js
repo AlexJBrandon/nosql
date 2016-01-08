c789fe6b430eaf28ad98091576566dae27e55c2a
mralexx <sandzio@o2.pl>
2016-01-08 07:19:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var connection = new Mongo();
-var db = connection.getDB('alex');
-var xela = db.xela.find();
-xela.forEach(function (result) {
+var db = connection.getDB('place');
+var places = db.places.find();
+places.forEach(function (result) {
     var loc = {"type": "Point","coordinates": [result.coordinate1, result.coordinate2]}
-    db.xela.update({_id: result._id},{ $set: { "loc": loc } });
-    db.xela.update({_id: result._id},{ $unset: { "coordinate1": "", "coordinate2": "", "type": "", "adress": "" } }
+    db.places.update({_id: result._id},{ $set: { "loc": loc } });
+    db.places.update({_id: result._id},{ $unset: { "coordinate1": "", "coordinate2": "", "type": "", "adress": "" } }
     );
 });
